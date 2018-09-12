0ae511ac61da262cb2336219daf7e25fbaa6c4e7
becklime <becklime@gmail.com>
2018-09-12 07:59:21 -0400

    Add leaflet map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+var dstu_map = L.map('mapid').setView([47.240085, 39.710701], 18);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets'
+}).addTo(dstu_map);
+
+
+
 var AppView = Backbone.View.extend({
 
 	el: '#container',
