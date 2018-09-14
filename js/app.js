1d56976904014f1ab0deb402f14dee8bca2f2530
becklime <becklime@gmail.com>
2018-09-14 09:29:27 -0400

    Add object models and views, try fetch objects

--- js/app.js
+++ js/app.js
@@ -1,11 +1,53 @@
-var dstu_map = L.map('mapid').setView([47.240085, 39.710701], 18);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets'
-}).addTo(dstu_map);
 
+
+var Types = Backbone.Model.extend({
+  defaults: function(){
+    return {
+      name: "Default type name",
+      description: "Default type description"
+    };
+  }
+});
+
+var Objects = Backbone.Model.extend({
+  defaults: function(){
+    return {
+      name: "Default object name",
+      description: "Default object description",
+      geom: {
+        'type': 'Point',
+        'coordinates': [
+             0.0,
+             0.0
+          ]
+      }
+     }
+   }
+});
+
+var ObjList = Backbone.Collection.extend({
+  model: Objects,
+  url:'http://194.87.110.73:8000/objects'
+  
+})
+
+var objlist = new ObjList();
+
+var ObjView = Marionette.View.extend({
+  tagName: "p",
+  initialize: function () {
+			this.listenTo(this.model, 'change', this.render);
+			this.listenTo(this.model, 'destroy', this.remove);
+			this.listenTo(this.model, 'visible', this.toggleVisible);
+		}
+});
+
+var ObjListView = Marionette.CollectionView.extend({
+  el: '#container',
+  model: Objects,
+  template: $('#template-layout').html()
+})
 
 
 var AppView = Backbone.View.extend({
@@ -17,6 +59,23 @@ var AppView = Backbone.View.extend({
 	},
 
 	render: function() {
-		this.$el.html("Hello World");
+    var dstu_map = L.map('mapid').setView([47.240085, 39.710701], 18);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'LimeBeck was here',
+      maxZoom: 19
+    }).addTo(dstu_map);
 	}
 });
+
+var App = Marionette.Application.extend({
+  region: '#container',
+  
+  onStart: function(){
+    objList = new ObjListView();
+    objList.render()
+  }
+});
+
+var app = new App();
+
+app.start()
